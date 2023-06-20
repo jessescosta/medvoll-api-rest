787392c3f6c2da2b35fee1195b80d6e11984f5b1
jessescosta <jesse.dossantoscosta@gmail.com>
2023-06-19 23:22:30 -0300

    Including JWT JSON Web Token in Pom.xml in Maven

--- src/main/java/med/voll/api/domain/usuario/AutenticacaoController.java
+++ src/main/java/med/voll/api/domain/usuario/AutenticacaoController.java
@@ -20,6 +20,6 @@ public class AutenticacaoController {
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados){
         var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
         var authentication = manager.authenticate(token);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok("token");
     }
 }
