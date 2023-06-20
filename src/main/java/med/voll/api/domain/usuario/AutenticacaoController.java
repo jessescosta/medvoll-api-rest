16f4b57f9aaac52e1ce9b0e5d196732a4418a487
jessescosta <jesse.dossantoscosta@gmail.com>
2023-06-20 19:21:47 -0300

    Authenticating the user and setting Spring's Filter preference

--- src/main/java/med/voll/api/domain/usuario/AutenticacaoController.java
+++ src/main/java/med/voll/api/domain/usuario/AutenticacaoController.java
@@ -23,6 +23,7 @@ public class AutenticacaoController {
     private TokenService tokenService;
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados){
+
         var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
         var authentication = manager.authenticate(authenticationToken);
 
