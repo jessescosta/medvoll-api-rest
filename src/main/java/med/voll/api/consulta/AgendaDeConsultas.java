5572223f5bf8e4b70e71f1697fad9d640b90a0fa
jessescosta <jesse.dossantoscosta@gmail.com>
2023-06-22 12:40:31 -0300

    Validate if there is a doctor and a patient and create a random choice of doctor.

--- src/main/java/med/voll/api/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voll/api/consulta/AgendaDeConsultas.java
@@ -1,10 +1,13 @@
 package med.voll.api.consulta;
 
+import jakarta.validation.ValidationException;
 import med.voll.api.domain.consulta.Consulta;
 import med.voll.api.domain.consulta.ConsultaRepository;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
+import med.voll.api.infra.exception.ValidacaoException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,10 +23,21 @@ public class AgendaDeConsultas {
 
     public void agendar(DadosAgendamentoConsulta dados) {
 
-        var medico = medicoRepository.findById(dados.idMedico()).get();
+        if (!pacienteRepository.existsById(dados.idPaciente())){
+            throw new ValidacaoException("Id do paciente ${dados.idPaciente()} informado não existe!");
+        }
+        if (dados.idMedico() != null  &&  !medicoRepository.existsById(dados.idMedico())){
+            throw new ValidacaoException("Id do médico  ${dados.idMedico()} informado não existe!");
+        }
+
+        var medico = escolherMedico(dados);
         var paciente = pacienteRepository.findById(dados.idPaciente()).get();
         var consulta = new Consulta(null, medico, paciente, dados.data());
         consultaRepository.save(consulta);
 
+    }
+
+    private Medico escolherMedico(DadosAgendamentoConsulta dados) {
+        return new Medico();
     }
 }
